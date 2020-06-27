f2f9b70d205a90279fd2ed23e60e51084af99a0e
Joe King <jking0186@gmail.com>
2020-06-27 14:54:49 -0500

    app setup/db setup

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jalepeño Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Swiss Burger", 0);
