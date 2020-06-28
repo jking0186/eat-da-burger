4e8d6423fff113d901acc429422cf3c7cdd110b5
Joe King <jking0186@gmail.com>
2020-06-28 15:44:33 -0500

    updated functions in orm.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(45) NOT NULL,
-    devoured BOOLEAN DEFAULT FALSE,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
