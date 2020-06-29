370f35caceaf6c3bd07232764cd2886b128ef232
Joe King <jking0186@gmail.com>
2020-06-29 09:48:35 -0500

    server connection

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 var express = require("express");
-var expresshb = require("express-handlebars");
+var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 
+var app = express();
+var connection = require("../config/connection.js")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
