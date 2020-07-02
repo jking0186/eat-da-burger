72ebfe20022153e5073d9cdf7d6f4a1db3c460c7
Joe King <jking0186@gmail.com>
2020-07-01 19:52:31 -0500

    Success

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(routes);
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-    // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
